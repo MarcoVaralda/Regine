ff85d07a7af6f986322ef87d2f9442a4cd2da6d8
marco <marco@192.168.1.7>
2021-04-12 10:04:43 +0200

    Soluzioni multiple

--- src/main/java/it/polito/tdp/Regine/model/TestRegine.java
+++ src/main/java/it/polito/tdp/Regine/model/TestRegine.java
@@ -3,15 +3,16 @@ package it.polito.tdp.Regine.model;
 import java.util.List;
 
 public class TestRegine {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Regine r = new Regine();
+	
+	public static void main(String args[]) {
+		Regine r = new Regine() ;
 		
-		List<Integer> soluzione;
-		soluzione = r.risolvi(6);
-		System.out.println(soluzione);
+		List<List<Integer>> soluzioni ;
+		soluzioni = r.risolvi(6);
+		System.out.println(soluzioni) ;
+
 	}
+	
+	
 
 }
